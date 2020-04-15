0fbe195a6ed5ee3005d4860e725ab13e54575b35
Endo <bmw523iboxing@gmail.com>
2020-04-15 13:22:57 +0900

    Q11

--- task.rb
+++ task.rb
@@ -95,9 +95,11 @@ end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
-
+   puts "ユーザーの趣味一覧"
   # 以下に回答を記載
-
+  sports.flatten.uniq.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+    end
 end
 
 def q12
